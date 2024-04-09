e8dca6db46fe20d71f66fc54ec3781dc9f8efa1a
snnmbb <nunnalin.au@gmail.com>
2024-04-09 14:31:04 +0700

    Add for loop for multiple image to code

--- Find-distance.py
+++ Find-distance.py
@@ -1,51 +1,60 @@
 import cv2
 import matplotlib.pyplot as plt
+from SolExDataCube import Dir_Read
+import os
+import re
+
+image_path= r"C:\Users\Asus\Desktop\Dot"
+os.chdir(image_path)
+
+pattern = re.compile(r'(\d+)\.png')
+
+for image_dot in Dir_Read('s', path=image_path):
+
+    image= cv2.imread(image_dot)
+
+    original_image= image
+
+    gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    edges= cv2.Canny(gray, 50,200)
+
+    try:  
+        contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 
-image= cv2.imread(r"C:\Users\Asus\Desktop\Dot\Dot2.png")
-original_image= image
+        sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
 
-gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-edges= cv2.Canny(gray, 50,200)
-
-try:  
-    contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        dot1= sorted_contours[0]
+        dot2= sorted_contours[1]
 
 
-    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
+        #dot1
+        R1= cv2.moments(dot1)
+        coordinate_center1= int(R1['m10']/R1['m00'])
 
-    dot1= sorted_contours[0]
-    dot2= sorted_contours[1]
+        print(image_dot)
+        print("center coordinate : ", str(coordinate_center1))
+        print("")
 
-
-    #dot1
-    R1= cv2.moments(dot1)
-
-    coordinate_center1= int(R1['m10']/R1['m00'])
-
-
-    print("center coordinate : ", str(coordinate_center1))
-
-    print("")
-
-    #dot2
-    R2= cv2.moments(dot2)
-
-    coordinate_center2= int(R2['m10']/R2['m00'])
+        #dot2
+        R2= cv2.moments(dot2)
+        coordinate_center2= int(R2['m10']/R2['m00'])
 
 
-    print("center coordinate : ", str(coordinate_center2))
+        print("center coordinate : ", str(coordinate_center2))
+        print("")
 
-    print("")
+        distance = coordinate_center1 - coordinate_center2
 
-    distance = coordinate_center1 - coordinate_center2
+        print("Distance between dot1 and dot2 : " , str(distance))
+        print("*****************")
+         
+        plt.imshow(image)
+        plt.show()
+    except:
+        print(image_dot)
+        print ("Circles are overlapping")
+        print("*****************")
 
-    print("Distance between dot1 and dot2 : " , str(distance))
-
-    plt.imshow(image)
-    plt.show()
-except:
-    print ("Circles are overlapping")
-
-#ใช้กับรูปที่วงกลมซ้อนทับกันกึ่งกลางไม้ได้
+    #ใช้กับรูปที่วงกลมซ้อนทับกันกึ่งกลางไม้ได้
