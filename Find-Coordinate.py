746882de84dd18a35fe5f5c3ac01fb22b8fd5601
snnmbb <nunnalin.au@gmail.com>
2024-04-09 13:47:00 +0700

    Add Try-Except for Overlapping condition

--- Find-Coordinate.py
+++ Find-Coordinate.py
@@ -1,34 +1,37 @@
 import cv2
 import matplotlib.pyplot as plt
+from SolExDataCube import Dir_Read
+import os
 
-
-image= cv2.imread(r"C:\Users\Asus\Desktop\Dot.png")
+image_path = r"C:\Users\Asus\Desktop\Dot"
+image= cv2.imread(r"C:\Users\Asus\Desktop\Dot")
+os.chdir(image_path)
 original_image= image
 
 gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 edges= cv2.Canny(gray, 50,200)
 
-
 contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
 
 sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
 
-item= sorted_contours[0]
+for image_dot in Dir_Read('s', path=image_path):
+    
+    item = sorted_contours[image_dot]
 
 
-#largest item
-R= cv2.moments(item)
+    #largest item
+    R= cv2.moments(item)
 
 
-coordinate_center= int(R['m10']/R['m00'])
+    coordinate_center= int(R['m10']/R['m00'])
 
 
-print("x center coordinate ", str(coordinate_center))
+    print("center coordinate ", str(coordinate_center))
 
-print("")
+    print("")
 
 
-plt.imshow(image)
-plt.show()
+    plt.imshow(image)
+    plt.show()
--- Find-distance.py
+++ Find-distance.py
@@ -2,46 +2,50 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-image= cv2.imread(r"C:\Users\Asus\Desktop\Dot3.png")
+image= cv2.imread(r"C:\Users\Asus\Desktop\Dot\Dot4.png")
 original_image= image
 
 gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
 edges= cv2.Canny(gray, 50,200)
 
-
-contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-
-sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
-
-dot1= sorted_contours[0]
-dot2= sorted_contours[1]
+try:  
+    contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 
-#dot1
-R1= cv2.moments(dot1)
+    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
 
-coordinate_center1= int(R1['m10']/R1['m00'])
-
-
-print("center coordinate : ", str(coordinate_center1))
-
-print("")
-
-#dot2
-R2= cv2.moments(dot2)
-
-coordinate_center2= int(R2['m10']/R2['m00'])
+    dot1= sorted_contours[0]
+    dot2= sorted_contours[1]
 
 
-print("center coordinate : ", str(coordinate_center2))
+    #dot1
+    R1= cv2.moments(dot1)
 
-print("")
+    coordinate_center1= int(R1['m10']/R1['m00'])
 
-distance = coordinate_center1 - coordinate_center2
 
-print("Distance between dot1 and dot2 : " , str(distance))
+    print("center coordinate : ", str(coordinate_center1))
 
-plt.imshow(image)
-plt.show()
+    print("")
+
+    #dot2
+    R2= cv2.moments(dot2)
+
+    coordinate_center2= int(R2['m10']/R2['m00'])
+
+
+    print("center coordinate : ", str(coordinate_center2))
+
+    print("")
+
+    distance = coordinate_center1 - coordinate_center2
+
+    print("Distance between dot1 and dot2 : " , str(distance))
+
+    plt.imshow(image)
+    plt.show()
+except:
+    print ("Circles are overlapping")
+
+#ใช้กับรูปที่วงกลมซ้อนทับกันกึ่งกลางไม้ได้
