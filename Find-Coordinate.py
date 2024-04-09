86e88690ff54104f0dc4dfe4f4a7cd8152e8797b
snnmbb <nunnalin.au@gmail.com>
2024-04-09 14:23:28 +0700

    Add for loop to code

--- Find-Coordinate.py
+++ Find-Coordinate.py
@@ -2,36 +2,44 @@ import cv2
 import matplotlib.pyplot as plt
 from SolExDataCube import Dir_Read
 import os
+import re
 
 image_path = r"C:\Users\Asus\Desktop\Dot"
-image= cv2.imread(r"C:\Users\Asus\Desktop\Dot")
 os.chdir(image_path)
-original_image= image
 
-gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-edges= cv2.Canny(gray, 50,200)
-
-contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
+pattern = re.compile(r'(\d+)\.png')
 
 for image_dot in Dir_Read('s', path=image_path):
     
-    item = sorted_contours[image_dot]
+    match = pattern.search(image_dot)
+    if match:
+        index = int(match.group(1))
+    else:
+        continue  # Skip this file if the numeric part cannot be extracted
+    
+    image= cv2.imread(image_dot)
+    
+    original_image= image
 
+    gray= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-    #largest item
-    R= cv2.moments(item)
+    edges= cv2.Canny(gray, 50,200)
 
+    contours, hierarchy= cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    coordinate_center= int(R['m10']/R['m00'])
-
-
-    print("center coordinate ", str(coordinate_center))
-
-    print("")
-
-
-    plt.imshow(image)
-    plt.show()
+    sorted_contours= sorted(contours, key=cv2.contourArea, reverse= False)
+    
+    if index < len(sorted_contours):
+        item = sorted_contours[index]
+        R = cv2.moments(item)
+        coordinate_center = int(R['m10'] / R['m00'])
+        
+        print("center coordinate ", str(coordinate_center))
+        print("")
+        
+        sorted_contours = 0
+        
+        plt.imshow(image)
+        plt.show()
+    else:
+        print("Index out of range for file:", image_dot)
