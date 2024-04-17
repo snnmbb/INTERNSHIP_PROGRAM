4509f39d6fd678762f32bc0bcfa6368a11d073c2
snnmbb <nunnalin.au@gmail.com>
2024-04-17 13:41:20 +0700

    Combine 2 array into 1 plot

--- 2D-Array.py
+++ 2D-Array.py
@@ -1,32 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-x = np.arange(0, 32)
-y = np.arange(0, 32)
+x = np.arange(0, 256)
+y = np.arange(0, 256)
 arr1 = np.zeros((y.size, x.size))
 arr2 = np.zeros((y.size, x.size))
 
 
-cx1 = 16 # x position
-cy1 = 16  # y position
-r1 = 10   # radius of circle
-cx2 = 4
-cy2 = 4
-r2 = 64
+cx1 = 64 # x position
+cy1 = 64  # y position
+r1 = 30   # radius of circle
+cx2 = 150
+cy2 = 150
+r2 = 30
 
 intensity1 = (x[np.newaxis,:]-cx1)**2 + (y[:,np.newaxis]-cy1)**2 < r1**2
 intensity2 = (x[np.newaxis,:]-cx2)**2 + (y[:,np.newaxis]-cy2)**2 < r2**2
 arr1[intensity1] = 50
 arr2[intensity2] = 200
+arr_total = arr1+arr2
 
+f = plt.figure(1)
+plt.imshow(arr_total)
 print(arr1, arr1.max(), arr1.min())
+print(arr2, arr2.max(), arr2.min())
 # print(arr1[intensity1])
 #print(arr2[intensity2])
-plt.figure(figsize=(5, 5))
+#plt.figure(figsize=(5, 5))
 # plt.pcolormesh(x, y, arr1)
-plt.imshow(arr1)
-
-#plt.pcolormesh(x, y, arr2)
-#plt.title("Intensity", loc='right')
-#plt.hist(arr1, bins = range(256))
 plt.show()
