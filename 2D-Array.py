268aa93cbb6ce06adfb66ef929269b144955b9bb
snnmbb <nunnalin.au@gmail.com>
2024-04-17 14:06:23 +0700

    add histogram plot

--- 2D-Array.py
+++ 2D-Array.py
@@ -18,11 +18,14 @@ r2 = 30
 intensity1 = (x[np.newaxis,:]-cx1)**2 + (y[:,np.newaxis]-cy1)**2 < r1**2
 intensity2 = (x[np.newaxis,:]-cx2)**2 + (y[:,np.newaxis]-cy2)**2 < r2**2
 arr1[intensity1] = 50
-arr2[intensity2] = 200
+arr2[intensity2] = 100
 arr_total = arr1+arr2
+counts, bins = np.histogram(arr_total)
 
-f = plt.figure(1)
+fig = plt.figure(1)
 plt.imshow(arr_total)
+fig2 = plt.figure(2)
+plt.hist(arr_total, bins='auto')
 print(arr1, arr1.max(), arr1.min())
 print(arr2, arr2.max(), arr2.min())
 # print(arr1[intensity1])
