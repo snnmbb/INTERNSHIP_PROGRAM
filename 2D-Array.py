6ca59597c05a734cf16c4cfa05b6761c12bdc17e
snnmbb <nunnalin.au@gmail.com>
2024-04-18 15:51:41 +0700

    add .flatten

--- 2D-Array.py
+++ 2D-Array.py
@@ -8,11 +8,11 @@ arr1 = np.zeros((y.size, x.size))
 arr2 = np.zeros((y.size, x.size))
 
 
-cx1 = 140# x position
-cy1 = 140  # y position
+cx1 = 160# x center position
+cy1 = 160  # y center position
 r1 = 40   # radius of circle
-cx2 = 150
-cy2 = 150
+cx2 = 140
+cy2 = 140
 r2 = 40
 
 intensity1 = (x[np.newaxis,:]-cx1)**2 + (y[:,np.newaxis]-cy1)**2 < r1**2
@@ -22,10 +22,16 @@ arr2[intensity2] = 100
 arr_total = arr1+arr2
 counts, bins = np.histogram(arr_total)
 
+distance_x = cx1-cx2
+distance_y = cy1-cy2
 fig = plt.figure(1)
 plt.imshow(arr_total)
+
 fig2 = plt.figure(2)
-plt.hist(arr_total, bins='auto')
+plt.hist(arr_total.flatten(), bins=30) #, bins='auto'
 print(arr1, arr1.max(), arr1.min())
 print(arr2, arr2.max(), arr2.min())
+print(arr_total, arr_total.max(), arr_total.min())
+print(distance_x)
+print(distance_y)
 plt.show()
