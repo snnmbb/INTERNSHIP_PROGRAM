558a2fc5d2f2fd2613efba6aee2ddd6942fb0de3
snnmbb <nunnalin.au@gmail.com>
2024-04-22 14:39:25 +0700

    Try

--- test.py
+++ test.py
@@ -4,41 +4,47 @@ import os
 import re
 
 
-image_path = r"C:\Users\Asus\Desktop\test_im2_1.png"
-image_path_new = r"C:\Users\Asus\Desktop\test_im2_w.png"
+image_path = r"C:\Users\Asus\Desktop\REF_CAP.png"
+image_path_new = r"C:\Users\Asus\Desktop\REF_CAP_W.png"
 pattern = re.compile(r'(\d+)\.png')
 directory = r'C:\Users\Asus\Desktop'
 os.chdir(directory) 
 image = cv2.imread(image_path)
 image_new = cv2.imread(image_path_new)
 original = image.copy()
+src = cv2.imread(image_path, 1) 
+
+#cv2.imshow("image",image)
+#cv2.waitKey()
 
 #save new image
 hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # rgb to hsv color space
 
 s_ch = hsv_img[:, :, 1]  # Get the saturation channel
 
-thesh = cv2.threshold(s_ch, 5, 255, cv2.THRESH_BINARY)[1]  # Apply threshold - pixels above 5 are going to be 255, other are zeros.
-thesh = cv2.morphologyEx(thesh, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7)))  # Apply opening morphological operation for removing artifacts.
+_,alpha = cv2.threshold(s_ch, 255, 0, cv2.THRESH_BINARY)  # Apply threshold - pixels above 5 are going to be 255, other are zeros.
 
-cv2.floodFill(thesh, None, seedPoint=(0, 0), newVal=128, loDiff=1, upDiff=1)  # Fill the background in thesh with the value 128 (pixel in the foreground stays 0.
+b, g, r = cv2.split(src)
 
-image[thesh == 128] = (255, 255, 255)  # Set all the pixels where thesh=128 to red.
+rgba = [b, g, r, alpha] 
 
-cv2.imwrite('test_im2_w.png', image)  # Save the output image.
+dst = cv2.merge(rgba, 4) 
+
+cv2.imwrite('REF_CAP_W.png', dst)  # Save the output image.
+
 
 #draw boundingbox
 gray = cv2.cvtColor(image_new, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
 
 ROI_number = 0
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
 for c in cnts:
     x, y, w, h = cv2.boundingRect(c)
-    cv2.rectangle(image_new, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(image_new, (x, y), (x + w, y + h), (0, 0, 255), 1)
 
-cv2.imshow("image", image_new)
+cv2.imshow("image", image)
 cv2.imshow("Thresh", thresh)
 cv2.waitKey()
