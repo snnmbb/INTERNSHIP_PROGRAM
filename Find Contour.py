dac96a012112c89e7683ad6d8fd1c7160fab284d
snnmbb <nunnalin.au@gmail.com>
2024-04-23 11:24:39 +0700

    Add bounding box

--- Find Contour.py
+++ Find Contour.py
@@ -5,22 +5,22 @@ import os
 import re
 from matplotlib import pyplot as plt
 
-image_path1 = r"C:\Users\Asus\Desktop\test_im2_1.png"
-image_path2 = r"C:\Users\Asus\Desktop\test_im2_ref.png"
+image_ref = r"C:\Users\Asus\Desktop\test_image_ref.png"
+image_path = r"C:\Users\Asus\Desktop\test_image.png"
 pattern = re.compile(r'(\d+)\.png')
 
 try:
     # Read file
-    dot1 = cv2.imread(image_path1)
-    dot2 = cv2.imread(image_path2)
+    dot1 = cv2.imread(image_ref)
+    dot2 = cv2.imread(image_path)
     
     # Convert color to grayscale
     gray_dot1 = cv2.cvtColor(dot1, cv2.COLOR_BGR2GRAY)
     gray_dot2 = cv2.cvtColor(dot2, cv2.COLOR_BGR2GRAY)
     
     # Find contour
-    ret, thresh1 = cv2.threshold(gray_dot1, 100, 255, cv2.THRESH_BINARY)
-    ret, thresh2 = cv2.threshold(gray_dot2, 100, 255, cv2.THRESH_BINARY)
+    ret, thresh1 = cv2.threshold(gray_dot1, 100, 500, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(gray_dot2, 100, 500, cv2.THRESH_BINARY)
 
     contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours2, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -30,20 +30,22 @@ try:
         cnt1 = contours1[0]
         cnt2 = contours2[0]
 
-        x1,y1,w1,h1 = cv2.boundingRect(cnt1)
-        x2,y2,w2,h2 = cv2.boundingRect(cnt2)
+        x_ref,y_ref,w_ref,h_ref = cv2.boundingRect(cnt1)
+        x,y,w,h = cv2.boundingRect(cnt2)
 
         # Create a mask
-        mask1 = cv2.drawContours(gray_dot1, [cnt1], 0, 255, thickness=cv2.FILLED)
-        mask2 = cv2.drawContours(gray_dot2, [cnt2], 0, 255, thickness=cv2.FILLED)
+        mask1 = cv2.drawContours(gray_dot1, [cnt1], -1, 255, thickness=-1)
+        mask2 = cv2.drawContours(gray_dot2, [cnt2], -1, 255, thickness=-1)
+        mask1 = cv2.rectangle(dot1, (x_ref, y_ref), (x_ref+w_ref, y_ref+h_ref), (255, 255, 0), 1)
+        mask2 = cv2.rectangle(dot2, (x, y), (x+w, y+h), (255, 255, 0), 1)
         
         # Bitwise AND operation
-        mask_combined = cv2.bitwise_and(mask1, mask2)
+        mask_and = cv2.bitwise_and(mask1, mask2)
         
         # Generate "exclusive" masks, i.e. masks without the intersection parts
-        mask1_excl = cv2.bitwise_xor(mask1, mask_combined)
-        mask2_excl = cv2.bitwise_xor(mask2, mask_combined)
-        mask1_excl = cv2.rectangle(mask1_excl, (x1, y1), (x1+w1, y1+h1), (255, 0, 0), 2)
+        mask1_excl = cv2.bitwise_xor(mask1, mask_and)
+        mask2_excl = cv2.bitwise_xor(mask2, mask_and)
+        mask2_ex = cv2.rectangle(mask2_excl, (x, y), (x+w_ref, y+h_ref), (255, 255, 0), 1)
 
         # Show images
         plt.figure(figsize=(10,6))
@@ -52,9 +54,9 @@ try:
         plt.subplot(3, 3, 3), plt.imshow(dot1+dot2, cmap='gray'), plt.xlabel('dot1+dot2')
         plt.subplot(3, 3, 4), plt.imshow(mask1, cmap='gray'), plt.xlabel('mask1')
         plt.subplot(3, 3, 5), plt.imshow(mask2, cmap='gray'), plt.xlabel('mask2')
-        plt.subplot(3, 3, 6), plt.imshow(mask_combined, cmap='gray'), plt.xlabel('mask_combined')
+        plt.subplot(3, 3, 6), plt.imshow(mask_and, cmap='gray'), plt.xlabel('mask_and')
         plt.subplot(3, 3, 7), plt.imshow(mask1_excl, cmap='gray'), plt.xlabel('mask1_excl')
-        plt.subplot(3, 3, 8), plt.imshow(mask2_excl, cmap='gray'), plt.xlabel('mask2_excl')
+        plt.subplot(3, 3, 8), plt.imshow(mask2_ex, cmap='gray'), plt.xlabel('mask2_excl')
         plt.show()
         
     else:
