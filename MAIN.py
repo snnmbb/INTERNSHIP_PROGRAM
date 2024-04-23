92a9474e656c36c200c30145e2b932c9902972d3
snnmbb <nunnalin.au@gmail.com>
2024-04-23 15:30:46 +0700

    EDIT

--- MAIN.py
+++ MAIN.py
@@ -140,92 +140,91 @@ def main():
             i += 1
             
             dot1 = cv2.imread(image_ref)
-        dot2 = cv2.imread(image_path)
+            dot2 = cv2.imread(image_path)
         
-        # Convert color to grayscale
-        gray_dot1 = cv2.cvtColor(dot1, cv2.COLOR_BGR2GRAY)
-        gray_dot2 = cv2.cvtColor(dot2, cv2.COLOR_BGR2GRAY)
-        
-        # Find contour
-        ret, thresh1 = cv2.threshold(gray_dot1, 100, 500, cv2.THRESH_BINARY)
-        ret, thresh2 = cv2.threshold(gray_dot2, 100, 500, cv2.THRESH_BINARY)
-
-        contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours2, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        if len(contours1) > 0 and len(contours2) > 0:
+            # Convert color to grayscale
+            gray_dot1 = cv2.cvtColor(dot1, cv2.COLOR_BGR2GRAY)
+            gray_dot2 = cv2.cvtColor(dot2, cv2.COLOR_BGR2GRAY)
             
-            cnt1 = contours1[0]
-            cnt2 = contours2[0]
+            # Find contour
+            ret, thresh1 = cv2.threshold(gray_dot1, 100, 500, cv2.THRESH_BINARY)
+            ret, thresh2 = cv2.threshold(gray_dot2, 100, 500, cv2.THRESH_BINARY)
 
-            x_ref,y_ref,w_ref,h_ref = cv2.boundingRect(cnt1)
-            x,y,w,h = cv2.boundingRect(cnt2)
+            contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours2, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-            # Create a mask
-            mask1 = cv2.drawContours(gray_dot1, [cnt1], -1, 255, thickness=-1)
-            mask2 = cv2.drawContours(gray_dot2, [cnt2], -1, 255, thickness=-1)
-            
-            # Bitwise AND operation
-            mask_and = cv2.bitwise_and(mask1, mask2)
-            
-            # Generate "exclusive" masks, i.e. masks without the intersection parts
-            mask1_excl = cv2.bitwise_xor(mask1, mask_and)
-            mask2_excl = cv2.bitwise_xor(mask2, mask_and)
-            mask2_ex = cv2.rectangle(mask2_excl, (x, y), (x+w_ref, y+h_ref), (255, 255, 0), 1)
+            if len(contours1) > 0 and len(contours2) > 0:
+                
+                cnt1 = contours1[0]
+                cnt2 = contours2[0]
 
-            #Find center coordinates and distance
-            cx_ref = ((x_ref+w_ref)+x_ref)/2   
-            cy_ref = ((y_ref+h_ref)+y_ref)/2      
-            center_x = ((x+w_ref)+x)/2
-            center_y = ((y+h_ref)+y)/2
-            distance_x = cx_ref-center_x
-            distance_y = cy_ref-center_y
-            
-            print('center of ref - x : ' + str(cx_ref) + ' , y : '+ str(cy_ref))
-            print('center of object - x : ' + str(center_x) + ' , y : '+ str(center_y))
-            print("Distance between objects x : " + str(distance_x) + " , y : " + str(distance_y))
-            
-            '''
-            #comment when using linear stage
-            # Show images
-            plt.figure(figsize=(10,6))
-            plt.subplot(3, 3, 1), plt.imshow(dot1, cmap='gray'), plt.xlabel('dot1')
-            plt.subplot(3, 3, 2), plt.imshow(dot2, cmap='gray'), plt.xlabel('dot2')
-            plt.subplot(3, 3, 3), plt.imshow(dot1+dot2, cmap='gray'), plt.xlabel('dot1+dot2')
-            plt.subplot(3, 3, 4), plt.imshow(mask1, cmap='gray'), plt.xlabel('mask1')
-            plt.subplot(3, 3, 5), plt.imshow(mask2, cmap='gray'), plt.xlabel('mask2')
-            plt.subplot(3, 3, 6), plt.imshow(mask_and, cmap='gray'), plt.xlabel('mask_and')
-            plt.subplot(3, 3, 7), plt.imshow(mask1_excl, cmap='gray'), plt.xlabel('mask1_excl')
-            plt.subplot(3, 3, 8), plt.imshow(mask2_ex, cmap='gray'), plt.xlabel('mask2_excl')
-            plt.show()
-            '''
-            reference = Decimal(0)
-            
-            while(True) :
-            #val_str = input("Enter value of offset : ")
-            #print("OK")
-            #val = Decimal(val_str)
-            #print("OK")
-                new_position = PID(Decimal(0.07) , Decimal(0.08), Decimal(0.01) , reference , Decimal(distance_x)) # KP , KI , KD , จุดที่แสงอยู่จุดศูนย์กลาง (reference 0) , ระยะห่างจากจุดศูนย์กลางที่รับค่าจากกล้อง/เซนเซอร์
-                print(new_position)
-                if new_position >= Decimal(0.01) :
-                    new_pos = pos-new_position
-                else :
-                    new_pos = pos+new_position  
-                print(new_pos)    
-                kcube.MoveTo(new_pos , 5000)
-                print(f'{kcube.Position}')
-            kcube.Home(60000)
-            print("Finished")
+                x_ref,y_ref,w_ref,h_ref = cv2.boundingRect(cnt1)
+                x,y,w,h = cv2.boundingRect(cnt2)
 
-            # Stop polling and close device
-            kcube.StopPolling()
-            kcube.Disconnect(True)
-        
-        else:
-            print("No contours found.")  
-        
-        
+                # Create a mask
+                mask1 = cv2.drawContours(gray_dot1, [cnt1], -1, 255, thickness=-1)
+                mask2 = cv2.drawContours(gray_dot2, [cnt2], -1, 255, thickness=-1)
+                
+                # Bitwise AND operation
+                mask_and = cv2.bitwise_and(mask1, mask2)
+                
+                # Generate "exclusive" masks, i.e. masks without the intersection parts
+                mask1_excl = cv2.bitwise_xor(mask1, mask_and)
+                mask2_excl = cv2.bitwise_xor(mask2, mask_and)
+                mask2_ex = cv2.rectangle(mask2_excl, (x, y), (x+w_ref, y+h_ref), (255, 255, 0), 1)
+
+                #Find center coordinates and distance
+                cx_ref = ((x_ref+w_ref)+x_ref)/2   
+                cy_ref = ((y_ref+h_ref)+y_ref)/2      
+                center_x = ((x+w_ref)+x)/2
+                center_y = ((y+h_ref)+y)/2
+                distance_x = cx_ref-center_x
+                distance_y = cy_ref-center_y
+                
+                print('center of ref - x : ' + str(cx_ref) + ' , y : '+ str(cy_ref))
+                print('center of object - x : ' + str(center_x) + ' , y : '+ str(center_y))
+                print("Distance between objects x : " + str(distance_x) + " , y : " + str(distance_y))
+                
+                '''
+                #comment when using linear stage
+                # Show images
+                plt.figure(figsize=(10,6))
+                plt.subplot(3, 3, 1), plt.imshow(dot1, cmap='gray'), plt.xlabel('dot1')
+                plt.subplot(3, 3, 2), plt.imshow(dot2, cmap='gray'), plt.xlabel('dot2')
+                plt.subplot(3, 3, 3), plt.imshow(dot1+dot2, cmap='gray'), plt.xlabel('dot1+dot2')
+                plt.subplot(3, 3, 4), plt.imshow(mask1, cmap='gray'), plt.xlabel('mask1')
+                plt.subplot(3, 3, 5), plt.imshow(mask2, cmap='gray'), plt.xlabel('mask2')
+                plt.subplot(3, 3, 6), plt.imshow(mask_and, cmap='gray'), plt.xlabel('mask_and')
+                plt.subplot(3, 3, 7), plt.imshow(mask1_excl, cmap='gray'), plt.xlabel('mask1_excl')
+                plt.subplot(3, 3, 8), plt.imshow(mask2_ex, cmap='gray'), plt.xlabel('mask2_excl')
+                plt.show()
+                '''
+                reference = Decimal(0)
+                
+                while(True) :
+                #val_str = input("Enter value of offset : ")
+                #print("OK")
+                #val = Decimal(val_str)
+                #print("OK")
+                    new_position = PID(Decimal(0.07) , Decimal(0.08), Decimal(0.01) , reference , Decimal(distance_x)) # KP , KI , KD , จุดที่แสงอยู่จุดศูนย์กลาง (reference 0) , ระยะห่างจากจุดศูนย์กลางที่รับค่าจากกล้อง/เซนเซอร์
+                    print(new_position)
+                    if new_position >= Decimal(0.01) :
+                        new_pos = pos-new_position
+                    else :
+                        new_pos = pos+new_position  
+                    print(new_pos)    
+                    kcube.MoveTo(new_pos , 5000)
+                    print(f'{kcube.Position}')
+            
+            else:
+                print("No contours found.")  
+        kcube.Home(60000)
+        print("Finished")
+
+        # Stop polling and close device
+        kcube.StopPolling()
+        kcube.Disconnect(True)
+            
 
         '''
         for i in range (10) :
