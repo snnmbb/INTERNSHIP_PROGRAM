1f66ee7176008d1871e15dcf20f7cfca7f1accf7
snnmbb <nunnalin.au@gmail.com>
2024-05-03 11:43:13 +0700

    Create capture() and Draw_Contour()

--- MAIN.py
+++ MAIN.py
@@ -7,13 +7,15 @@ from matplotlib import pyplot as plt
 import zwoasi as asi
 import time
 import sys
+from SolExDataCube import Dir_Read
 import clr
 
 image_ref = r"C:\Users\Asus\Desktop\test_image_ref.png"
-save_path_mono = r"C:\Users\Asus\Desktop\ASI1600-PRO\img\mono\\"
-save_path_mono16 = r"C:\Users\Asus\Desktop\ASI1600-PRO\img\mono16\\"
+save_path = r"C:\Users\Asus\Desktop\LAB_TEST\REAL_DATA"
 asi.init('C:\\Users\\Asus\\AppData\\Local\\Programs\\Python\\Python310\\Lib\\ASI SDK\\lib\\x64\ASICamera2.lib')
 pattern = re.compile(r'(\d+)\.png')
+os.chdir(save_path)
+
 
 num_cameras = asi.get_num_cameras()
 if num_cameras == 0:
@@ -128,105 +130,148 @@ def main():
             raise
 
         for i in range(20) :
-            print('Capturing a single 8-bit mono image')
-            filename = str(i)+'_image_mono.jpg'
-            camera.set_image_type(asi.ASI_IMG_RAW8)
-            camera.capture(filename=save_path_mono+filename)
-            print('Saved to %s' % filename)
-
-            print('Capturing a single 16-bit mono image')
-            filename = str(i)+'_image_mono16.tiff'
-            camera.set_image_type(asi.ASI_IMG_RAW16)
-            camera.capture(filename=save_path_mono16+filename)
-            print('Saved to %s' % filename)
-            i += 1
             
-            dot1 = cv2.imread(image_ref)
-            dot2 = cv2.imread(image_path)
-        
-            # Convert color to grayscale
-            gray_dot1 = cv2.cvtColor(dot1, cv2.COLOR_BGR2GRAY)
-            gray_dot2 = cv2.cvtColor(dot2, cv2.COLOR_BGR2GRAY)
+            capture()
             
-            # Find contour
-            ret, thresh1 = cv2.threshold(gray_dot1, 100, 500, cv2.THRESH_BINARY)
-            ret, thresh2 = cv2.threshold(gray_dot2, 100, 500, cv2.THRESH_BINARY)
-
-            contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            contours2, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-            if len(contours1) > 0 and len(contours2) > 0:
-                
-                cnt1 = contours1[0]
-                cnt2 = contours2[0]
-
-                x_ref,y_ref,w_ref,h_ref = cv2.boundingRect(cnt1)
-                x,y,w,h = cv2.boundingRect(cnt2)
-
-                # Create a mask
-                mask1 = cv2.drawContours(gray_dot1, [cnt1], -1, 255, thickness=-1)
-                mask2 = cv2.drawContours(gray_dot2, [cnt2], -1, 255, thickness=-1)
-                
-                # Bitwise AND operation
-                mask_and = cv2.bitwise_and(mask1, mask2)
-                
-                # Generate "exclusive" masks, i.e. masks without the intersection parts
-                mask1_excl = cv2.bitwise_xor(mask1, mask_and)
-                mask2_excl = cv2.bitwise_xor(mask2, mask_and)
-                mask2_ex = cv2.rectangle(mask2_excl, (x, y), (x+w_ref, y+h_ref), (255, 255, 0), 1)
-
-                #Find center coordinates and distance
-                cx_ref = ((x_ref+w_ref)+x_ref)/2   
-                cy_ref = ((y_ref+h_ref)+y_ref)/2      
-                center_x = ((x+w_ref)+x)/2
-                center_y = ((y+h_ref)+y)/2
-                distance_x = cx_ref-center_x
-                distance_y = cy_ref-center_y
-                
-                print('center of ref - x : ' + str(cx_ref) + ' , y : '+ str(cy_ref))
-                print('center of object - x : ' + str(center_x) + ' , y : '+ str(center_y))
-                print("Distance between objects x : " + str(distance_x) + " , y : " + str(distance_y))
-                
-                '''
-                #comment when using linear stage
-                # Show images
-                plt.figure(figsize=(10,6))
-                plt.subplot(3, 3, 1), plt.imshow(dot1, cmap='gray'), plt.xlabel('dot1')
-                plt.subplot(3, 3, 2), plt.imshow(dot2, cmap='gray'), plt.xlabel('dot2')
-                plt.subplot(3, 3, 3), plt.imshow(dot1+dot2, cmap='gray'), plt.xlabel('dot1+dot2')
-                plt.subplot(3, 3, 4), plt.imshow(mask1, cmap='gray'), plt.xlabel('mask1')
-                plt.subplot(3, 3, 5), plt.imshow(mask2, cmap='gray'), plt.xlabel('mask2')
-                plt.subplot(3, 3, 6), plt.imshow(mask_and, cmap='gray'), plt.xlabel('mask_and')
-                plt.subplot(3, 3, 7), plt.imshow(mask1_excl, cmap='gray'), plt.xlabel('mask1_excl')
-                plt.subplot(3, 3, 8), plt.imshow(mask2_ex, cmap='gray'), plt.xlabel('mask2_excl')
-                plt.show()
-                '''
-                
+            for save_path in Dir_Read('s', path=save_path):
+                Draw_Contour()
                 reference = Decimal(0)
-                
+                    
                 while(True) :
-                    new_position = PID(Decimal(0.07) , Decimal(0.08), Decimal(0.01) , reference , Decimal(distance_x)) # KP , KI , KD , จุดที่แสงอยู่จุดศูนย์กลาง (reference 0) , ระยะห่างจากจุดศูนย์กลางที่รับค่าจากกล้อง/เซนเซอร์
+                    new_position = PID(Decimal(0.07) , Decimal(0.08), Decimal(0.01) , reference , Decimal(disX_nor)) # KP , KI , KD , จุดที่แสงอยู่จุดศูนย์กลาง (reference 0) , ระยะห่างจากจุดศูนย์กลางที่รับค่าจากกล้อง/เซนเซอร์
                     print(new_position)
                     if new_position >= Decimal(0.01) :
                         new_pos = pos-new_position
                     else :
                         new_pos = pos+new_position  
-                    print(new_pos)    
-                    kcube.MoveTo(new_pos , 5000)
-                    print(f'{kcube.Position}')
-            
-            else:
-                print("No contours found.")  
+                        print(new_pos)    
+                        kcube.MoveTo(new_pos , 5000)
+                        print(f'{kcube.Position}')
+                
         kcube.Home(60000)
         print("Finished")
 
         # Stop polling and close device
         kcube.StopPolling()
         kcube.Disconnect(True)
+        
+        
                  
     except Exception as e:
         print("ERROR:", e)   
+        
+def capture() :
+    for j in range(10):
+        print("----------------------------------------------")
+        print('Capturing image')
+        filename = str(j)+'_image_lab.png'
+        camera.set_image_type(asi.ASI_IMG_RAW16)
+        camera.capture(filename=save_path+filename)
+        print('Saved to %s' % filename)
+        print("----------------------------------------------")
+        j=+1
 
+def Draw_Contour() :
+    dot1 = cv2.imread(image_ref)
+    dot2 = cv2.imread(image_path)
+        
+    wid = dot1.shape[1] 
+    hgt = dot1.shape[0] 
+        
+    print('image pixels size = ' , str(wid) + " x " + str(hgt))
+    
+    # Convert color to grayscale
+    gray_dot1 = cv2.cvtColor(dot1, cv2.COLOR_BGR2GRAY)
+    gray_dot2 = cv2.cvtColor(dot2, cv2.COLOR_BGR2GRAY)
+    combine_dot = cv2.bitwise_or(gray_dot1, gray_dot2)
+        
+    # Find contour
+    ret, thresh1 = cv2.threshold(gray_dot1, 100, 500, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(gray_dot2, 100, 500, cv2.THRESH_BINARY)
 
+    contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours2, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours1) > 0 and len(contours2) > 0:   
+        cnt1 = contours1[0]
+        cnt2 = contours2[0]
+
+        x_ref,y_ref,w_ref,h_ref = cv2.boundingRect(cnt1)
+        x,y,w,h = cv2.boundingRect(cnt2)
+            
+        print('w_ref = ',w_ref)
+        print('h_ref = ',h_ref)
+        print('x_ref = ',x_ref)
+        print('y_ref = ',y_ref)
+
+        print('w = ',w)
+        print('h = ',h)
+        print('x = ',x)
+        print('y = ',y)
+
+        # Create a mask
+        mask1 = cv2.drawContours(gray_dot1, [cnt1], -1, 255, thickness=-1)
+        mask2 = cv2.drawContours(gray_dot2, [cnt2], -1, 255, thickness=-1)
+            
+        # Bitwise AND operation
+        mask_and = cv2.bitwise_and(mask1, mask2)
+        mask_or = cv2.bitwise_or(mask1,mask2)
+            
+        # Generate "exclusive" masks, i.e. masks without the intersection parts
+        mask1_excl = cv2.bitwise_xor(mask1, mask_and)
+        mask2_excl = cv2.bitwise_xor(mask2, mask_and)
+        mask2_ex = cv2.rectangle(mask2_excl, (x, y), (x+w, y+h), (255, 255, 0), 1)
+
+        #Find center coordinates and distance
+        cx_ref = ((x_ref+w_ref)+x_ref)/2   
+        cy_ref = ((y_ref+h_ref)+y_ref)/2      
+        center_x = ((x+w_ref)+x)/2
+        center_y = ((y+h_ref)+y)/2
+        distance_x = cx_ref-center_x
+        distance_y = cy_ref-center_y
+            
+            #print('sum', len(np.argwhere(mask_or == np.amax(mask_or))))
+            
+        print("-------------------------------------------------")
+        print(save_path)
+        print('center of ref - x : ' + str(cx_ref) + ' , y : '+ str(cy_ref))
+        print('center of object - x : ' + str(center_x) + ' , y : '+ str(center_y))
+        print("Distance between objects - x : " + str(distance_x) + " , y : " + str(distance_y))
+
+        # Normalized
+        CX_ref_nor = cx_ref/4656
+        CY_ref_nor = cy_ref/3520
+        center_x_nor = center_x/4656
+        center_y_nor = center_y/3520
+        disX_nor = distance_x/4656
+        disY_nor = distance_y/3520
+            
+        print("--------------------Normalize--------------------")
+        print('CX_ref = ' , CX_ref_nor)
+        print('CY_ref = ' , CY_ref_nor)
+        print('center_x = ' , center_x_nor)
+        print('center_Y = ' , center_y_nor)
+        print('disX = ' , disX_nor)
+        print('disY = ' , disY_nor)
+        print("-------------------------------------------------")
+                
+        '''
+        #comment when using linear stage
+        # Show images
+        plt.figure(figsize=(10,6))
+        plt.subplot(3, 3, 1), plt.imshow(dot1, cmap='gray'), plt.xlabel('dot1')
+        plt.subplot(3, 3, 2), plt.imshow(dot2, cmap='gray'), plt.xlabel('dot2')
+        plt.subplot(3, 3, 3), plt.imshow(dot1+dot2, cmap='gray'), plt.xlabel('dot1+dot2')
+        plt.subplot(3, 3, 4), plt.imshow(mask1, cmap='gray'), plt.xlabel('mask1')
+        plt.subplot(3, 3, 5), plt.imshow(mask2, cmap='gray'), plt.xlabel('mask2')
+        plt.subplot(3, 3, 6), plt.imshow(mask_and, cmap='gray'), plt.xlabel('mask_and')
+        plt.subplot(3, 3, 7), plt.imshow(mask1_excl, cmap='gray'), plt.xlabel('mask1_excl')
+        plt.subplot(3, 3, 8), plt.imshow(mask2_ex, cmap='gray'), plt.xlabel('mask2_excl')
+        plt.show()
+        '''
+        return disX_nor
+    else:
+         print("No contours found.")  
+        
 if __name__ == "__main__":
     main()
