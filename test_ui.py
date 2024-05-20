bc6c9659079a74e213f5247511d971bef896bd89
snnmbb <nunnalin.au@gmail.com>
2024-05-20 14:39:09 +0700

    Edit

--- test_ui.py
+++ test_ui.py
@@ -1,39 +1,39 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
- 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
- 
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(150, 70, 93, 28))
- 
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(130, 149, 151, 31))
-        self.label.setText("")
- 
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-        
-        # adding signal and slot
-        self.pushButton.clicked.connect(self.showmsg) 
- 
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "Click"))
-         
-    def showmsg(self):
-        # slot
-        self.label.setText("You clicked me")
- 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
- 
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Dialog()
- 
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+import tkinter as tk
+from tkinter import ttk
+
+def button_func():
+    print("Kp : " + entry_kp.get())
+    print("Ki : " + entry_ki.get())
+    print("Kd : " + entry_kd.get())
+    print("First Position : " + entry_pos.get())
+    
+# Window setting
+window = tk.Tk()
+window.title('PID TUNING')
+window.geometry('600x400')
+
+# Title
+title_label_kp = ttk.Label(master=window, text='Kp = ', font='Calibri 12')
+title_label_kp.place(x = 50 , y = 100)
+title_label_ki = ttk.Label(master=window, text='Ki = ', font='Calibri 12')
+title_label_ki.place(x = 50 , y = 150)
+title_label_kd = ttk.Label(master=window, text='Kd = ', font='Calibri 12')
+title_label_kd.place(x = 50 , y = 200)
+title_label_pos = ttk.Label(master=window, text='First Position = ', font='Calibri 12')
+title_label_pos.place(x = 50 , y = 250)
+
+# Input field
+entry_kp = ttk.Entry(master=window)
+entry_kp.place(x = 150 , y = 100)
+entry_ki = ttk.Entry(master=window)
+entry_ki.place(x = 150 , y = 150)
+entry_kd = ttk.Entry(master=window)
+entry_kd.place(x = 150 , y = 200)
+entry_pos = ttk.Entry(master=window)
+entry_pos.place(x = 150 , y = 250)
+# Button
+button = ttk.Button(master=window, text='Enter', command=button_func)
+button.place(x = 400 , y = 100)
+
+# Run the application
+window.mainloop()
