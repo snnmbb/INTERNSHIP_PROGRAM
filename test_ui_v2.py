5652e8d33edd1e3ac0c4b0539be08c9d5d818794
snnmbb <nunnalin.au@gmail.com>
2024-05-21 10:46:57 +0700

    Create new file and test (completed)

---
+++ test_ui_v2.py
@@ -0,0 +1,252 @@
+import cv2 as cv
+import cv2
+import numpy as np
+import os
+import re
+from matplotlib import pyplot as plt
+import time
+import sys
+from SolExDataCube import Dir_Read
+import csv
+import tkinter as tk
+from tkinter import ttk
+
+
+image_ref = r"C:\Users\Asus\Desktop\LAB_TEST\REF\REF.png"
+save_path = r"C:\\Users\\Asus\\Desktop\LAB_TEST\DATA2"
+pattern = re.compile(r'(\d+)\.png')
+os.chdir(save_path)
+
+e_prev = 0
+
+def PID(Kp , Ki , Kd , setpoint , measurement ): # measurement เป็นตำแหน่งที่จุด offset จากจุดศูนย์กลาง รับค่าจากกล้อง/เซนเซอร์....
+    global time_prev, e_prev# Value of offset - when the error is equal zero
+    # PID calculations
+    e = setpoint - measurement
+    P = Kp*e
+    I = Ki*(e+e_prev)
+    D = Kd*(e-e_prev) 
+    pid = P + I + D
+    # update stored data for next iteration
+    e_prev = e
+    time_prev = time
+    return pid
+
+def Draw_Contour(path) :
+    dot1 = cv2.imread(image_ref)
+    dot2 = cv2.imread(path)
+        
+    wid = dot1.shape[1] 
+    hgt = dot1.shape[0] 
+        
+    print('image pixels size = ' , str(wid) + " x " + str(hgt))
+    
+    # Convert color to grayscale
+    gray_dot1 = cv2.cvtColor(dot1, cv2.COLOR_BGR2GRAY)
+    gray_dot2 = cv2.cvtColor(dot2, cv2.COLOR_BGR2GRAY)
+    combine_dot = cv2.bitwise_or(gray_dot1, gray_dot2)
+        
+    # Find contour
+    ret, thresh1 = cv2.threshold(gray_dot1, 100, 500, cv2.THRESH_BINARY)
+    ret, thresh2 = cv2.threshold(gray_dot2, 100, 500, cv2.THRESH_BINARY)
+
+    contours1, _ = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours2, _ = cv2.findContours(thresh2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours1) > 0 and len(contours2) > 0:   
+        cnt1 = contours1[0]
+        cnt2 = contours2[0]
+
+        x_ref,y_ref,w_ref,h_ref = cv2.boundingRect(cnt1)
+        x,y,w,h = cv2.boundingRect(cnt2)
+            
+        print('w_ref = ',w_ref)
+        print('h_ref = ',h_ref)
+        print('x_ref = ',x_ref)
+        print('y_ref = ',y_ref)
+
+        print('w = ',w)
+        print('h = ',h)
+        print('x = ',x)
+        print('y = ',y)
+
+        # Create a mask
+        mask1 = cv2.drawContours(gray_dot1, [cnt1], -1, 255, thickness=-1)
+        mask2 = cv2.drawContours(gray_dot2, [cnt2], -1, 255, thickness=-1)
+            
+        # Bitwise AND operation
+        mask_and = cv2.bitwise_and(mask1, mask2)
+        mask_or = cv2.bitwise_or(mask1,mask2)
+            
+        # Generate "exclusive" masks, i.e. masks without the intersection parts
+        mask1_excl = cv2.bitwise_xor(mask1, mask_and)
+        mask2_excl = cv2.bitwise_xor(mask2, mask_and)
+        mask2_ex = cv2.rectangle(mask2_excl, (x, y), (x+w, y+h), (255, 255, 0), 1)
+
+        #Find center coordinates and distance
+        cx_ref = ((x_ref+w_ref)+x_ref)/2   
+        cy_ref = ((y_ref+h_ref)+y_ref)/2      
+        center_x = ((x+w_ref)+x)/2
+        center_y = ((y+h_ref)+y)/2
+        distance_x = cx_ref-center_x
+        distance_y = cy_ref-center_y
+                        
+        print("-------------------------------------------------")
+        print('center of ref - x : ' + str(cx_ref) + ' , y : '+ str(cy_ref))
+        print('center of object - x : ' + str(center_x) + ' , y : '+ str(center_y))
+        print("Distance between objects - x : " + str(distance_x) + " , y : " + str(distance_y))
+
+        # Normalized
+        CX_ref_nor = cx_ref*0.00038
+        CY_ref_nor = cy_ref*0.00038
+        center_x_nor = center_x*0.00038
+        center_y_nor = center_y*0.00038
+        disX_nor = distance_x*0.00038
+        disY_nor = distance_y*0.00038
+            
+        print("--------------------Normalize--------------------")
+        print('CX_ref = ' , CX_ref_nor)
+        print('CY_ref = ' , CY_ref_nor)
+        print('center_x = ' , center_x_nor)
+        print('center_Y = ' , center_y_nor)
+        print('disX = ' , disX_nor)
+        print('disY = ' , disY_nor)
+        print("-------------------------------------------------")
+                
+        '''
+        #comment when using linear stage
+        # Show images
+        plt.figure(figsize=(10,6))
+        plt.subplot(3, 3, 1), plt.imshow(dot1, cmap='gray'), plt.xlabel('dot1')
+        plt.subplot(3, 3, 2), plt.imshow(dot2, cmap='gray'), plt.xlabel('dot2')
+        plt.subplot(3, 3, 3), plt.imshow(dot1+dot2, cmap='gray'), plt.xlabel('dot1+dot2')
+        plt.subplot(3, 3, 4), plt.imshow(mask1, cmap='gray'), plt.xlabel('mask1')
+        plt.subplot(3, 3, 5), plt.imshow(mask2, cmap='gray'), plt.xlabel('mask2')
+        plt.subplot(3, 3, 6), plt.imshow(mask_and, cmap='gray'), plt.xlabel('mask_and')
+        plt.subplot(3, 3, 7), plt.imshow(mask1_excl, cmap='gray'), plt.xlabel('mask1_excl')
+        plt.subplot(3, 3, 8), plt.imshow(mask2_ex, cmap='gray'), plt.xlabel('mask2_excl')
+        plt.show()
+        '''
+        return disX_nor
+    else:
+         print("No contours found.")  
+
+error=[]
+kp = 0
+ki = 0
+kd = 0
+pos = 0
+
+
+def main() :
+    
+    
+    try :
+       
+        
+        #-------------------------------WINDOW VERSION----------------------------------------------
+
+            
+        
+        # Window setting
+        window = tk.Tk()
+        window.title('PID TUNING')
+        window.geometry('600x400')
+
+        # Title
+        title_label_kp = ttk.Label(master=window, text='Kp = ', font='Calibri 12')
+        title_label_kp.place(x = 50 , y = 100)
+        title_label_ki = ttk.Label(master=window, text='Ki = ', font='Calibri 12')
+        title_label_ki.place(x = 50 , y = 150)
+        title_label_kd = ttk.Label(master=window, text='Kd = ', font='Calibri 12')
+        title_label_kd.place(x = 50 , y = 200)
+        title_label_pos = ttk.Label(master=window, text='First Position = ', font='Calibri 12')
+        title_label_pos.place(x = 50 , y = 250)
+
+        # Input field
+        entry_kp = ttk.Entry(master=window)
+        entry_kp.place(x = 150 , y = 100)
+        entry_ki = ttk.Entry(master=window)
+        entry_ki.place(x = 150 , y = 150)
+        entry_kd = ttk.Entry(master=window)
+        entry_kd.place(x = 150 , y = 200)
+        entry_pos = ttk.Entry(master=window)
+        entry_pos.place(x = 150 , y = 250)
+        
+        def button_func():
+            global KP
+            global KI
+            global KD
+            global POS
+            KP = entry_kp.get()
+            KI  = entry_ki.get()
+            KD = entry_kd.get()
+            POS = entry_pos.get()
+            print("Kp : " + entry_kp.get())
+            print("Ki : " + entry_ki.get())
+            print("Kd : " + entry_kd.get())
+            print("First Position : " + entry_pos.get())
+            
+        def start() :
+            kp = float(KP)
+            ki = float(KI)
+            kd = float(KD)
+            pos = float(POS)
+            new_pos = []
+            
+            for path in Dir_Read('s', path=save_path):
+                print("----------------------------------------------")
+                print('Capturing image')
+                print("----------------------------------------------")                    
+                time.sleep(0.5)
+                disX = Draw_Contour(path)
+                reference = 0 #จุดที่แสงอยู่จุดศูนย์กลาง               
+                err = PID(kp, ki, kd, reference , disX) # KP , KI , KD , จุดที่แสงอยู่จุดศูนย์กลาง (reference 0) , ระยะห่างจากจุดศูนย์กลางที่รับค่าจากกล้อง/เซนเซอร์
+                print("Error : " + str(err))
+                if err > reference :
+                    new_position = pos-err
+                    print("New_position : " + str(new_position)   ) 
+                elif err < reference: 
+                    new_position = pos+err
+                    print("New_position : " + str(new_position)    )
+                else :
+                    break
+                time.sleep(0.5)
+                error.append(err)
+                new_pos.append(new_position)
+                    
+                plt.subplot(1, 2, 1)
+                plt.plot(error)
+                plt.xlabel('Index')
+                plt.ylabel('ERR')
+                    
+                plt.subplot(1, 2, 2)
+                plt.plot(new_pos)
+                plt.xlabel('Index')
+                plt.ylabel('NEW_POSITION')
+                    
+                plt.tight_layout()
+                plt.gca().invert_yaxis()
+                plt.show()
+                
+            #def Reset():
+                
+        # Button
+        button_enter = ttk.Button(master=window, text='ENTER', command=button_func)
+        button_enter.place(x = 400 , y = 100)
+        button_run = ttk.Button(master=window, text='START', command=start)
+        button_run.place(x = 400 , y = 200)
+        #button_reset = ttk.Button(master=window, text='RESET', command=reset)
+        #button_reset.place(x = 400 , y = 200)
+
+        # Run the application
+        window.mainloop()
+        
+        
+            
+            
+    except Exception as e:
+        print("ERROR:", e)
+
+if __name__ == "__main__":
+    main()   
