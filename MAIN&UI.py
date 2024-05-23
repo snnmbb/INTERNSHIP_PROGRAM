93aaf327307857aaaffa8503e04a64da04fa1936
snnmbb <nunnalin.au@gmail.com>
2024-05-23 13:38:09 +0700

    Add ui

--- MAIN&UI.py
+++ MAIN&UI.py
@@ -12,6 +12,8 @@ import clr
 import sys
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+from tkinter import font
 
 
 #-----------------------------------------------SETUP CAMERA--------------------------------------------------------------------
@@ -238,28 +240,36 @@ def main():
         #-------------------------------WINDOW VERSION----------------------------------------------
         # Window setting
         window = tk.Tk()
-        window.title('PID TUNING')
-        window.geometry('600x400')
+        window.geometry("600x400") 
+        bg = PhotoImage(file = 'C://Users//Asus//Desktop//INTERNSHIP_PROGRAM//BG_UI.png')
+        canvas1 = Canvas( window, width = 600, height = 400)  
+        canvas1.pack(fill = "both", expand = True) 
+        canvas1.create_image( 0, 0, image = bg,anchor = "nw") 
+
+        rectangle = canvas1.create_rectangle(50, 90, 350, 140, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
+        rectangle = canvas1.create_rectangle(50, 150, 350, 200, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
+        rectangle = canvas1.create_rectangle(50, 210, 350, 260, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
+        rectangle = canvas1.create_rectangle(50, 270, 350, 320, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
 
         # Title
-        title_label_kp = ttk.Label(master=window, text='Kp = ', font='Calibri 12')
-        title_label_kp.place(x = 50 , y = 100)
-        title_label_ki = ttk.Label(master=window, text='Ki = ', font='Calibri 12')
-        title_label_ki.place(x = 50 , y = 150)
-        title_label_kd = ttk.Label(master=window, text='Kd = ', font='Calibri 12')
-        title_label_kd.place(x = 50 , y = 200)
-        title_label_pos = ttk.Label(master=window, text='First Position = ', font='Calibri 12')
-        title_label_pos.place(x = 50 , y = 250)
+        title_label_kp = ttk.Label(master=window, text='KP', background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
+        title_label_kp.place(x = 60 , y = 90)
+        title_label_ki = ttk.Label(master=window, text='KI',background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
+        title_label_ki.place(x = 60 , y = 150)
+        title_label_kd = ttk.Label(master=window, text='KD',background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
+        title_label_kd.place(x = 60 , y = 210)
+        title_label_pos = ttk.Label(master=window, text='FIRST POSITION',background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
+        title_label_pos.place(x = 60 , y = 270)
 
         # Input field
-        entry_kp = ttk.Entry(master=window)
-        entry_kp.place(x = 150 , y = 100)
-        entry_ki = ttk.Entry(master=window)
-        entry_ki.place(x = 150 , y = 150)
-        entry_kd = ttk.Entry(master=window)
-        entry_kd.place(x = 150 , y = 200)
-        entry_pos = ttk.Entry(master=window)
-        entry_pos.place(x = 150 , y = 250)
+        entry_kp = ttk.Entry(master=window , background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center")
+        entry_kp.place(x = 60 , y = 105 , width= 280, height=30)
+        entry_ki = ttk.Entry(master=window, background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center" )
+        entry_ki.place(x = 60 , y = 165, width= 280, height=30)
+        entry_kd = ttk.Entry(master=window, background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center")
+        entry_kd.place(x = 60 , y = 225 , width= 280, height=30)
+        entry_pos = ttk.Entry(master=window, background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center" )
+        entry_pos.place(x = 60 , y = 285 , width= 280, height=30)
         
         def Enter():
             global KP
@@ -368,13 +378,77 @@ def main():
                     time.sleep(0.1)
                 i+=1
         
-        button_default = ttk.Button(master=window, text='DEFAULT', command=Default)
-        button_default.place(x = 400 , y = 100)       
-        button_enter = ttk.Button(master=window, text='ENTER', command=Enter)
-        button_enter.place(x = 400 , y = 150)
-        button_run = ttk.Button(master=window, text='START', command=Start)
-        button_run.place(x = 400 , y = 200)
+        # Button
+        button = tk.Button(master=window, text="DEFAULT", 
+                        command=Default,
+                        activebackground="dodgerblue4", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="slategray3",
+                        cursor="hand2",
+                        disabledforeground="lightsteelblue1",
+                        fg="black",
+                        font=("CenturyGothic", 12),
+                        height=1,
+                        highlightbackground="white",
+                        highlightcolor="lightgray",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100 )
+        button.place(x = 400 , y = 125)
 
+        button = tk.Button(master=window, text="ENTER", 
+                        command=Enter,
+                        activebackground="dodgerblue4", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="slategray3",
+                        cursor="hand2",
+                        disabledforeground="lightsteelblue1",
+                        fg="black",
+                        font=("CenturyGothic", 12),
+                        height=1,
+                        highlightbackground="white",
+                        highlightcolor="lightgray",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100 )
+        button.place(x = 400 , y = 185)
+
+        button = tk.Button(master=window, text="START", 
+                        command=Start,
+                        activebackground="dodgerblue4", 
+                        activeforeground="white",
+                        anchor="center",
+                        bd=3,
+                        bg="slategray3",
+                        cursor="hand2",
+                        disabledforeground="lightsteelblue1",
+                        fg="black",
+                        font=("CenturyGothic", 12),
+                        height=1,
+                        highlightbackground="white",
+                        highlightcolor="lightgray",
+                        highlightthickness=2,
+                        justify="center",
+                        overrelief="raised",
+                        padx=10,
+                        pady=5,
+                        width=15,
+                        wraplength=100 )
+        button.place(x = 400 , y = 245)
+
+        window.mainloop()
                     
     except Exception as e:
         print("ERROR:", e)    
