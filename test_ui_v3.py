1b398f164ea61d30d8a9188f7a31da2d2a318374
snnmbb <nunnalin.au@gmail.com>
2024-06-04 15:16:39 +0700

    Edit

--- test_ui_v3.py
+++ test_ui_v3.py
@@ -156,42 +156,27 @@ def main() :
         window.geometry("600x400") 
         window.title("Translation stage control 2024 V.0.1")
         
-        bg = PhotoImage(file = 'C://Users//Asus//Desktop//INTERNSHIP_PROGRAM//BG_UI.png')
+        bg = PhotoImage(file = 'C://Users//Asus//Desktop//INTERNSHIP_PROGRAM//UI_v3.png')
         canvas1 = Canvas( window, width = 600, height = 400)  
         canvas1.pack(fill = "both", expand = True) 
         canvas1.create_image( 0, 0, image = bg,anchor = "nw") 
 
-        rectangle = canvas1.create_rectangle(50, 90, 350, 140, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
-        rectangle = canvas1.create_rectangle(50, 150, 350, 200, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
-        rectangle = canvas1.create_rectangle(50, 210, 350, 260, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
-        rectangle = canvas1.create_rectangle(50, 270, 350, 320, outline = "gray89", fill = "gray89") #(x1,y1,x2,y2)
-
-        # Title
-        title_label_kp = ttk.Label(master=window, text='KP', background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
-        title_label_kp.place(x = 60 , y = 90)
-        title_label_ki = ttk.Label(master=window, text='KI',background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
-        title_label_ki.place(x = 60 , y = 150)
-        title_label_kd = ttk.Label(master=window, text='KD',background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
-        title_label_kd.place(x = 60 , y = 210)
-        title_label_pos = ttk.Label(master=window, text='FIRST POSITION',background ="gray89", font='CenturyGothic 10 bold' , foreground ="gray")
-        title_label_pos.place(x = 60 , y = 270)
-
         # Input field
         entry_kp = ttk.Entry(master=window , background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center")
         entry_kp.insert(0,"32")
-        entry_kp.place(x = 60 , y = 105 , width= 280, height=30)      
+        entry_kp.place(x = 305 , y = 120, width= 120, height=30)      
         
         entry_ki = ttk.Entry(master=window, background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center" )
         entry_ki.insert(0,"0.5")
-        entry_ki.place(x = 60 , y = 165, width= 280, height=30)
+        entry_ki.place(x = 305 , y = 175, width= 120, height=30)
         
         entry_kd = ttk.Entry(master=window, background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center")
         entry_kd.insert(0,"0.5")
-        entry_kd.place(x = 60 , y = 225 , width= 280, height=30)
+        entry_kd.place(x = 305 , y = 230 , width= 120, height=30)
         
         entry_pos = ttk.Entry(master=window, background = "lightsteelblue4", foreground ="dodgerblue4" , justify= "center" )
         entry_pos.insert(0,"60")
-        entry_pos.place(x = 60 , y = 285 , width= 280, height=30)
+        entry_pos.place(x = 305 , y = 290 , width= 120, height=30)
         
         
         def stop() :
@@ -289,9 +274,9 @@ def main() :
                         overrelief="raised",
                         padx=10,
                         pady=5,
-                        width=15,
+                        width=10,
                         wraplength=100 )
-        button.place(x = 400 , y = 100)
+        button.place(x = 460 , y = 120)
         
         button = tk.Button(master=window, text="ENTER", 
                         command=enter,
@@ -312,9 +297,9 @@ def main() :
                         overrelief="raised",
                         padx=10,
                         pady=5,
-                        width=15,
+                        width=10,
                         wraplength=100 )
-        button.place(x = 400 , y = 150)
+        button.place(x = 460 , y = 170)
 
         button = tk.Button(master=window, text="START", 
                         command=start_app,
@@ -335,9 +320,9 @@ def main() :
                         overrelief="raised",
                         padx=10,
                         pady=5,
-                        width=15,
+                        width=10,
                         wraplength=100 )
-        button.place(x = 400 , y = 200)
+        button.place(x = 460 , y = 220)
 
         button = tk.Button(master=window, text="STOP", 
                         command=stop,
@@ -358,9 +343,9 @@ def main() :
                         overrelief="raised",
                         padx=10,
                         pady=5,
-                        width=15,
+                        width=10,
                         wraplength=100 )
-        button.place(x = 400 , y = 250)
+        button.place(x = 460 , y = 270)
         
         # Run the application
         window.mainloop()
